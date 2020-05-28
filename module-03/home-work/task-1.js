878c916867d8727f6bf0679134dc346fd1d54a79
Ruslan Tisanyk <ruslantisanykot@gmail.com>
2020-05-28 17:48:36 +0300

    task-1 completed

--- module-03/home-work/task-1.js
+++ module-03/home-work/task-1.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+
+const keys = Object.keys(user);
+
+// eslint-disable-next-line
+for (const key of keys) {
+  console.log(`${key}: ${user[key]}`);
+}
