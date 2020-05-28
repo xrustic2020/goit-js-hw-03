2725194712f4e51a569d09766671c8bac84894b9
Ruslan Tisanyk <ruslantisanykot@gmail.com>
2020-05-28 17:59:18 +0300

    task 2 completed

--- module-03/home-work/task-1.js
+++ module-03/home-work/task-1.js
@@ -1,5 +1,9 @@
 'use strict';
 
+console.log('========================================');
+console.log('| task 1');
+console.log('========================================');
+
 const user = {
   name: 'Mango',
   age: 20,
---
+++ module-03/home-work/task-2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+console.log('========================================');
+console.log('| task 2');
+console.log('========================================');
+
+const countProps = function(obj) {
+  return Object.values(obj).length;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
